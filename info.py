bd2827390885ef74d86af4815ebcdaa5879a903f
Mohit Kumar <100722700+Mohit18110@users.noreply.github.com>
2024-09-12 20:47:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -148,7 +148,7 @@ STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or Flase
 
 # Online Stream and Download
 NO_PORT = bool(environ.get('NO_PORT', False))
-APP_NAME = None
+APP_NAME = 'test2bot-613b307fbde2'
 if 'DYNO' in environ:
     ON_HEROKU = True
     APP_NAME = environ.get('APP_NAME')
